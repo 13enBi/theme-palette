288921e516f74df519c63ac2834dc7dc1d1d6e75
yuzefeng <yuzefeng>
2020-11-09 08:56:33 +0800

    dev

--- palette-frontend/src/components/Color/ClassBtn.vue
+++ palette-frontend/src/components/Color/ClassBtn.vue
@@ -7,49 +7,43 @@
 </template>
 
 <script lang="ts">
-import { ref, onMounted, computed, watchEffect } from 'vue';
-import { clipboardWrite } from '../../common/utils';
-import { message } from 'ant-design-vue';
+	import { ref, onMounted, computed, watchEffect } from 'vue';
+	import { clipboardWrite } from '../../common/utils';
+	import { message } from 'ant-design-vue';
 
-export default {
-	props: {
-		classList: Set,
-		type: { type: String, default: 'other' },
-		colorName: { type: String, default: '' },
-	},
-	setup(props) {
-		const handleCopy = (className: string) => {
-			const str = [props.type, className, props.colorName].reduce((total, curr) =>
-				curr ? (total += `-${curr}`) : total,
-            );
-            
-            watchEffect(() => {
-               
-            })
+	export default {
+		props: {
+			classList: Set,
+			type: { type: String, default: 'other' },
+			colorName: { type: String, default: '' },
+		},
+		setup(props) {
+			const handleCopy = (className: string) => {
+				const str = [props.type, className, props.colorName].reduce((total, curr) =>
+					curr ? (total += `-${curr}`) : total,
+				);
 
-            
+				clipboardWrite(str as string);
 
-			clipboardWrite(str as string);
+				message.success(`copied： ${str} !`);
+			};
 
-			message.success(`copied： ${str} !`);
-		};
-
-		return { handleCopy, classList: props.classList };
-	},
-};
+			return { handleCopy, classList: props.classList };
+		},
+	};
 </script>
 
 <style lang="less">
-.class-btn {
-	display: flex;
-	gap: 5px;
-	justify-content: center;
-	align-items: center;
-	padding: 0 5px;
-	z-index: 249;
-	button {
-		width: 31%;
-		margin: 0 5px;
+	.class-btn {
+		display: flex;
+		gap: 5px;
+		justify-content: center;
+		align-items: center;
+		padding: 0 5px;
+		z-index: 249;
+		button {
+			width: 31%;
+			margin: 0 5px;
+		}
 	}
-}
 </style>
